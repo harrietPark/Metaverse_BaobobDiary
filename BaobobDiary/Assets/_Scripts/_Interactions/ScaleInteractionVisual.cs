491c22804a0274cd60451a1e27523db24afd2532
Seonjeong Park <29harriet@naver.com>
2024-06-30 19:26:30 +0100

    to the end of the meditattion and the first pass of the transition

--- BaobobDiary/Assets/_Scripts/_Interactions/ScaleInteractionVisual.cs
+++ BaobobDiary/Assets/_Scripts/_Interactions/ScaleInteractionVisual.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ScaleInteractionVisual : MonoBehaviour
 {
     [SerializeField] LineRenderer lineRenderer;
-    [SerializeField] GameObject leftHandSphere;
+    [SerializeField] GameObject leftHandSphere; 
     [SerializeField] GameObject rightHandSphere;
+
+    [SerializeField] Texture _defaultIcon;
+    [SerializeField] Texture _changedIcon;
 
     private bool isGestureActive = false;
 
@@ -14,16 +18,44 @@ public class ScaleInteractionVisual : MonoBehaviour
     [SerializeField] float maxScale;
     [SerializeField] GameObject scaledObject;
 
+    private float maxScaleTimer = 0f;
+    private const float maxScaleDuration = 1f;
+
+    [HideInInspector] public UnityEvent maxScaledForThreeSecs;
+
+    private void Start()
+    {
+        
+    }
+
+    public void ShowRectile()
+    {
+        leftHandSphere.SetActive(true);
+        rightHandSphere.SetActive(true);
+    }
+
+    public void DisableRectile()
+    {
+        leftHandSphere.SetActive(false);
+        rightHandSphere.SetActive(false);
+    }
+
     public void ShowLineRenderer()
     {
         lineRenderer.enabled = true;
         isGestureActive = true;
+
+        leftHandSphere.GetComponent<MeshRenderer>().material.mainTexture = _changedIcon;
+        rightHandSphere.GetComponent<MeshRenderer>().material.mainTexture = _changedIcon;
     }
 
     public void DisableLineRenderer()
     {
         lineRenderer.enabled = false;
         isGestureActive = false;
+
+        leftHandSphere.GetComponent<MeshRenderer>().material.mainTexture = _defaultIcon;
+        rightHandSphere.GetComponent<MeshRenderer>().material.mainTexture = _defaultIcon;
     }
 
     private void Update()
@@ -37,6 +69,20 @@ public class ScaleInteractionVisual : MonoBehaviour
             float scaleFactor = Mathf.Clamp(distance, minScale, maxScale);
 
             scaledObject.transform.localScale = new Vector3(scaleFactor, scaleFactor, scaleFactor);
+
+            if(Mathf.Approximately(scaleFactor, maxScale))
+            {
+                maxScaleTimer += Time.deltaTime;
+                if(maxScaleTimer >= maxScaleDuration)
+                {
+                    maxScaledForThreeSecs.Invoke();
+                    maxScaleTimer = 0f;
+                }
+            }
+            else
+            {
+                maxScaleTimer = 0f;
+            }
         }
     }
 }
--- BaobobDiary/Assets/_Scripts/_Meditation/MeditationController.cs
+++ BaobobDiary/Assets/_Scripts/_Meditation/MeditationController.cs
@@ -6,6 +6,8 @@ using UnityEngine.Playables;
 using UnityEngine.Timeline;
 using Meta.XR.MRUtilityKit;
 using static UnityEngine.GraphicsBuffer;
+using UnityEngine.SceneManagement;
+using EasyTransition;
 
 public class MeditationController : MonoBehaviour
 {
@@ -18,7 +20,7 @@ public class MeditationController : MonoBehaviour
     [Tooltip("Assets")]
     [SerializeField] GameObject tutorialHand;
     [SerializeField] GameObject oneTwoThreeUI;
-    [SerializeField] GameObject sphere;
+    [SerializeField] GameObject bubble;
 
     [SerializeField] Transform playerTransform;
     [SerializeField] float tutorialHandDistance;
@@ -42,6 +44,8 @@ public class MeditationController : MonoBehaviour
     [HideInInspector] public UnityEvent onNarration7Finished;
 
     [HideInInspector] public UnityEvent onNarration8Finished;
+    [HideInInspector] public UnityEvent onNarration9Finished;
+    [HideInInspector] public UnityEvent onNarration10Finished;
 
     [HideInInspector] public UnityEvent onOneTwoThreeFinished;
 
@@ -50,16 +54,23 @@ public class MeditationController : MonoBehaviour
     private bool isButtonPressed = false;
     Animator tutorialHandAnimator;
     GameObject tutorialHandInstance;
-    GameObject bubbleInstance;
 
     [SerializeField] ActiveStateGroup poseActivateStateGroup;
     [SerializeField] ActiveStateGroup gestureActivateStateGroup;
+    [SerializeField] ActiveStateGroup scalePoseActiveStateGroup;
 
     [SerializeField] private OVRPassthroughLayer _passthroughLayer;
+    [SerializeField] ScaleInteractionVisual scaleInteractionVisual;
+
+    public TransitionSettings transition;
+    public float loadDelay;
 
     private void Start()
     {
         tutorialHandAnimator = tutorialHand.GetComponent<Animator>();
+        //bubble.SetActive(false);
+        
+        scaleInteractionVisual.maxScaledForThreeSecs.AddListener(OnMaxScaledForThreeSecs);
     }
 
     //bind this to the meditation start button
@@ -73,7 +84,7 @@ public class MeditationController : MonoBehaviour
             StartCoroutine(AdjustLighting(-0.5f, 4f));
             //어서와. (pause) 지금부터 잠시 머리를 비워볼거야.(pause)
             //몸의 긴장을 풀고, 지금 이 순간에 정신을 집중해봐.
-            PlayNarration(0, onNarration0Finished, false, null);
+            PlayNarration(0, onNarration0Finished, false, null, false);
         }
         
     }
@@ -89,7 +100,6 @@ public class MeditationController : MonoBehaviour
             {
                 elapsedTime += Time.deltaTime;
                 float newBrightness = Mathf.Lerp(startBrightness, targetBrightness, elapsedTime / duration);
-                Debug.Log($"Adjusting lighting with brightness: {newBrightness}");
                 _passthroughLayer.SetBrightnessContrastSaturation(newBrightness);
                 yield return null;
             }
@@ -108,16 +118,16 @@ public class MeditationController : MonoBehaviour
         meditationBGAudioSource.Play();
     }
 
-    private void PlayNarration(int narrationIndex, UnityEvent onComplete, bool checkGesture, ActiveStateGroup activateStateGroup)
+    private void PlayNarration(int narrationIndex, UnityEvent onComplete, bool checkGesture, ActiveStateGroup activateStateGroup, bool passOneTwoThree)
     {
         meditationNarrationAudioSource.clip = narrations[narrationIndex];
         meditationNarrationAudioSource.Play();
         leftHand.sharedMaterial = defaultMat;
         rightHand.sharedMaterial = defaultMat;
-        StartCoroutine(WaitForNarrationToEnd(narrations[narrationIndex].length, onComplete, checkGesture, activateStateGroup));
+        StartCoroutine(WaitForNarrationToEnd(narrations[narrationIndex].length, onComplete, checkGesture, activateStateGroup, passOneTwoThree));
     }
 
-    IEnumerator WaitForNarrationToEnd(float duration, UnityEvent onComplete, bool checkGesture, ActiveStateGroup activateStateGroup)
+    IEnumerator WaitForNarrationToEnd(float duration, UnityEvent onComplete, bool checkGesture, ActiveStateGroup activateStateGroup, bool passOneTwoThree)
     {
         if (checkGesture)
         {
@@ -126,14 +136,48 @@ public class MeditationController : MonoBehaviour
 
         yield return new WaitForSeconds(duration);
 
-        if (checkGesture)
+        if (checkGesture && !passOneTwoThree)
         {
             yield return StartCoroutine(WaitForAdditionalTime(3.0f, onComplete));
         }
-        else
+        else if(!checkGesture)
         {
             onComplete.Invoke();
         }
+        else if(checkGesture && passOneTwoThree)
+        {
+            StartCoroutine(WaitMore(3f, onComplete));
+        }
+    }
+
+    private IEnumerator WaitMore(float additionalTime, UnityEvent onComplete)
+    {
+        float elapsedTime = 0.0f;
+
+        while(elapsedTime < additionalTime)
+        {
+            if (isGestureCorrect)
+            {
+                isGestureCorrect = false;
+                onComplete.Invoke();
+                yield break;
+            }
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        while (true)
+        {
+            if (isGestureCorrect)
+            {
+                isGestureCorrect = false;
+                onComplete.Invoke();
+                yield break;
+            }
+            yield return null;
+        }
+        //다시 하라는 UI
+
     }
 
     private IEnumerator WaitForAdditionalTime(float additionalTime, UnityEvent onComplete)
@@ -207,6 +251,8 @@ public class MeditationController : MonoBehaviour
         onNarration7Finished.AddListener(OnNarration7Finished);
 
         onNarration8Finished.AddListener(OnNarration8Finished);
+        onNarration9Finished.AddListener(OnNarration9Finished);
+        onNarration10Finished.AddListener(OnNarration10Finished);
 
         //onOneTwoThreeFinished.AddListener(OnOneTwoThreeFinished);
     }
@@ -223,6 +269,8 @@ public class MeditationController : MonoBehaviour
         onNarration7Finished.RemoveListener(OnNarration7Finished);
 
         onNarration8Finished.RemoveListener(OnNarration8Finished);
+        onNarration9Finished.RemoveListener(OnNarration9Finished);
+        onNarration10Finished.RemoveListener(OnNarration10Finished);
 
         //onOneTwoThreeFinished.RemoveListener(OnOneTwoThreeFinished);
     }
@@ -237,7 +285,9 @@ public class MeditationController : MonoBehaviour
     private void InstantiateBubble()
     {
         Vector3 bubblePose = playerTransform.position + playerTransform.forward * bubbleDistance + bubbleOffset;
-        bubbleInstance = Instantiate(sphere, bubblePose, Quaternion.LookRotation(playerTransform.forward));
+        bubble.transform.position = bubblePose;
+        bubble.transform.rotation = Quaternion.LookRotation(playerTransform.forward);
+        bubble.SetActive(true);
     }
 
     private void OnNarration0Finished()
@@ -245,7 +295,7 @@ public class MeditationController : MonoBehaviour
         //narration1 starts
         //눈 앞에 두 손 보이지? (손 wave)
         //두 손을 따라하며 천천히 같이 호흡을 해보자.
-        PlayNarration(1, onNarration1Finished, false, null);
+        PlayNarration(1, onNarration1Finished, false, null, false);
         //tutorial hand shows up in front of the player 
         InstantiateTutorialHand();
         tutorialHandAnimator.SetTrigger("wave");
@@ -257,7 +307,7 @@ public class MeditationController : MonoBehaviour
     {
         //narration2 starts
         //자, 손을 동그랗게 벌리며 숨을 크게 들이 쉬어봐. 숨을 참아야 해
-        PlayNarration(2, onNarration2Finished, true, poseActivateStateGroup);
+        PlayNarration(2, onNarration2Finished, true, poseActivateStateGroup, false);
         StartCoroutine(AnimationDelay(3.0f, "circlePose"));
     }
 
@@ -271,7 +321,7 @@ public class MeditationController : MonoBehaviour
     {
         //narration3 starts
         //그리고 그대로 숨을 끝까지 내뱉으면서 손을 앞으로 뻗어보자. 
-        PlayNarration(3, onNarration3Finished, true, gestureActivateStateGroup);
+        PlayNarration(3, onNarration3Finished, true, gestureActivateStateGroup, false);
         StartCoroutine(AnimationDelay(3.0f, "pushCircle"));
     }
 
@@ -279,7 +329,7 @@ public class MeditationController : MonoBehaviour
     {
         //narration4 starts
         //잘했어 다시 한 번. 손에 집중해서 숨을 들이쉬자. 
-        PlayNarration(4, onNarration4Finished, true, poseActivateStateGroup);
+        PlayNarration(4, onNarration4Finished, true, poseActivateStateGroup, false);
         StartCoroutine(AnimationDelay(0.0f, "thumbsUp"));
     }
 
@@ -287,7 +337,7 @@ public class MeditationController : MonoBehaviour
     {
         //narration5 starts
         //잠시 머금고 내뱉어. 
-        PlayNarration(5, onNarration5Finished, true, gestureActivateStateGroup);
+        PlayNarration(5, onNarration5Finished, true, gestureActivateStateGroup, false);
         StartCoroutine(AnimationDelay(0.0f, "pushCircle"));
     }
 
@@ -295,7 +345,7 @@ public class MeditationController : MonoBehaviour
     {
         //narration6 starts
         //좋아 마지막이야. 호흡이 오가는 그 길을 느껴봐. 
-        PlayNarration(6, onNarration6Finished, true, poseActivateStateGroup);
+        PlayNarration(6, onNarration6Finished, true, poseActivateStateGroup, false);
         StartCoroutine(AnimationDelay(0.0f, "thumbsUp"));
     }
 
@@ -303,13 +353,13 @@ public class MeditationController : MonoBehaviour
     {
         //narration7 starts
         //깊게 내뱉으며 손을 모아줘. 
-        PlayNarration(7, onNarration7Finished, true, gestureActivateStateGroup);
+        PlayNarration(7, onNarration7Finished, true, gestureActivateStateGroup, false);
         StartCoroutine(AnimationDelay(0.0f, "pushCircle"));
     }
 
     private void OnNarration7Finished()
     {
-        StartCoroutine(ShowBubble(2.0f, 2.0f));
+        StartCoroutine(ShowBubble(1.0f, 2.0f));
         StartCoroutine(AdjustLighting(0.0f, 4f));
     }
 
@@ -319,11 +369,40 @@ public class MeditationController : MonoBehaviour
         yield return new WaitForSeconds(delay1);
         InstantiateBubble();
         yield return new WaitForSeconds(delay2);
-        PlayNarration(8, onNarration8Finished, false, null);
+        PlayNarration(8, onNarration8Finished, false, null, false);
+        
     }
 
     private void OnNarration8Finished()
     {
+        //세번째 손가락과 엄지를 모아봐. 
+        PlayNarration(9, onNarration9Finished, true, scalePoseActiveStateGroup, true);
+    }
 
+    private void OnNarration9Finished()
+    {
+        //그리고 당겨봐. 당길수있는 최대한으로. 
+        meditationNarrationAudioSource.clip = narrations[10];
+        meditationNarrationAudioSource.Play();
+        scaleInteractionVisual.maxScaledForThreeSecs.AddListener(OnMaxScaledForThreeSecs);
+    }
+
+    private void OnNarration10Finished()
+    {
+        //그리고 3초를 버텨. 하나 둘 셋 
+        //PlayNarration(11, null, false, null, true);
+    }
+
+    private void OnMaxScaledForThreeSecs()
+    {
+        //scene transtiion
+        Debug.Log("scene transition happens ehere");
+        // fadeIn.SetTrigger("triggerTransition");
+        LoadScene("SJ_TestScene");
+    }
+
+    private void LoadScene(string _sceneName)
+    {
+        TransitionManager.Instance().Transition(_sceneName, transition, loadDelay);
     }
 }
